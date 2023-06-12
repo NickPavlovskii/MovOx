7b964a74d85345fe06bbe8511abbd20cc4650470
NickPavlovskii <84456340+NickPavlovskii@users.noreply.github.com>
2023-06-12 15:53:42 +0300

    хранилещн

--- kinox-app/src/main.js
+++ kinox-app/src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store/store'; // Import the Vuex store
 
+// import VueCarousel from 'vue-carousel';
+// import Vue from 'vue';
+// Vue.use(VueCarousel);
+
 const app = createApp(App);
 app.use(router);
 app.use(store); // Use the Vuex store
 
 app.mount('#app');
+
