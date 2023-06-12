0f910e057c0cfd2bda6aeb1d201fda969d37b37e
NickPavlovskii <84456340+NickPavlovskii@users.noreply.github.com>
2023-06-12 15:52:47 +0300

    главная страница

--- kinox-app/src/components/MovieList.vue
+++ kinox-app/src/components/MovieList.vue
@@ -1,23 +1,72 @@
 <template>
   <div>
     <ContentWrapper @search="performSearch" class="ContentWrapper" />
-    <div class="container">
+
+    <div v-if="!searchQuery">
+      <WatchNow />
+      <TopMovie />
+      <MyRecom/>
+      
+    </div>
+
+    <div class="container" v-if="searchQuery">
+      <h2>Наща коллекция</h2>
+      <p>  Вы любите смотреть фильмы онлайн и проводите много времени, прочесывая сайты в поисках чего-нибудь интересного?
+      Тогда вам стоит задержаться у нас</p>
+    
       <ul class="movie-list">
-        <li v-for="(movie, index) in displayedMovies" :key="movie.externalId.kpHD" :class="{'movie-item': true, 'new-row': index % 5 === 0}">
+        <li v-for="(movie, index) in displayedMovies" :key="movie.externalId.kpHD" :class="{'movie-item': true, 'new-row': index % 5 === 0}" style="  cursor: pointer;text-decoration: none; list-style-type: none;">
+
+              <router-link to="/" style="  cursor: pointer;text-decoration: none; list-style-type: none;"> 
           <div class="movie-poster">
-            <img :src="movie.poster.url" alt="Постер фильма" class="poster-image" />
+           
+                <img :src="movie.poster.url" alt="Постер фильма" class="poster-image" />
+
+            <font-awesome-icon icon="heart" class="heart"/>
           </div>
+      
           <div class="movie-details">
-            <h2 class="movie-name">{{ movie.name }}</h2>
-            <div class="movie-info">
-              <div class="rating-circle">
-                <div class="circle" :style="{ backgroundColor: getColor(movie.rating.kp) }">
-                  {{ movie.rating.kp }}
-                </div>
-              </div>
+            <circle-progress class="circle_progress"
+            :viewport="true"
+        
+            :on-viewport= "movie.rating.kp.toFixed(1)"
+                :size="60"
+                :background="'white'"
+      :is-gradient="true"
+      :percent=movie.rating.kp*10
+      :gradient="{
+        angle: 90,
+        startColor: '#ff0000',
+        stopColor: '#ffff00'
+    }"
+    :is-bg-shadow="true"
+      :bg-shadow="{
+        inset: true,
+        vertical: 2,
+        horizontal: 2,
+        blur: 4,
+        opacity: .4,
+        color: '#000000'
+    }"
+    :border-width="5"
+      :border-bg-width="5"
+  >
+
+        
+  </circle-progress>
+  <span  class="ratingtext">
+        {{ movie.rating.kp.toFixed(1) }}
+      </span>  
+
+            <h3 class="movie-name">{{ movie.name }} </h3>
+             <!-- ({{ movie.type}}) -->
+       
+             
               <div class="year">{{ movie.year }}</div>
-            </div>
+          
           </div>
+        </router-link>
+         
         </li>
       </ul>
 
@@ -27,12 +76,26 @@
 </template>
 
 <script>
-import ContentWrapper from './ContentWrapper.vue';
-import { mapState, mapActions } from 'vuex';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import ContentWrapper from './main/ContentWrapper.vue';
+import  TopMovie from './main/TopMovie.vue';
+import   WatchNow from './main/WatchNow.vue';
+import   MyRecom from './main/MyRecom.vue';
 
+import { mapState, mapActions } from 'vuex';
+import CircleProgress from "vue3-circle-progress";
+import { faBookmark, faHeart } from '@fortawesome/free-solid-svg-icons'
+library.add(faBookmark)
+library.add(faHeart)
 export default {
   components: {
     ContentWrapper,
+    CircleProgress,
+    FontAwesomeIcon,
+    TopMovie,
+    WatchNow,
+    MyRecom
   },
   data() {
     return {
@@ -62,21 +125,42 @@ export default {
       this.$store.commit('setSearchQuery', query); // Использовать мутацию setSearchQuery из хранилища Vuex
       this.currentPage = 1;
     },
-    getColor(rating) {
-      if (rating < 5) {
-        return "#ff0000"; // red
-      } else if (rating < 8) {
-        return "#ffa500"; // orange
-      } else {
-        return "#008000"; // green
-      }
-    },
+    // getColor(rating) {
+    //   if (rating < 5) {
+    //     return ['#ff0000']; // red
+    //   } else if (rating < 8) {
+    //     return ['#ffa500']; // orange
+    //   } else {
+    //     return ['#008000']; // green
+    //   }
+    // },
   },
 };
 </script>
 
 
 <style scoped>
+
+.circle_progress{
+  position: absolute;
+  bottom: 70px;
+  left: 10px;
+  color: #ffffff;
+
+}
+.circle_progress .percentage{
+  color: black;
+}
+
+.ratingtext {
+  position: relative;
+bottom: 112px;
+margin-left: 26px;
+  font-size: 21px;
+color: #020c1b;
+  font-weight: bold;
+  text-align: center;
+}
 .container {
   max-width: 960px;
   margin: 0 auto;
@@ -103,27 +187,44 @@ export default {
 }
 
 .movie-item {
-  border: 1px solid #ccc;
+
   padding: 10px;
-  border-radius: 5px;
+
 }
 
 .movie-poster {
   text-align: center;
+  border-radius: 8px;
 }
+.heart{
+  position: relative;
+  bottom: 280px;
+  left: 80px;
+ color: #020c1b;
+}
+
+
 
 .poster-image {
   max-width: 100%;
   height: auto;
+  border-radius: 11px;
 }
 
 .movie-details {
   padding-top: 10px;
+  width: 100%;
+  color: white;
+  height: 80px;
 }
 
 .movie-name {
-  font-size: 18px;
+  font-size: 19px;
   margin-bottom: 5px;
+  position: relative;
+  bottom: 90px;
+
+  font-weight: bold;
 }
 
 .movie-info {
@@ -131,85 +232,20 @@ export default {
   align-items: center;
 }
 
-.rating-circle {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  background-color: #f5a623;
-  color: #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 18px;
-  font-weight: bold;
-  margin-right: 10px;
-}
+
 
 .year {
   font-size: 16px;
   color: #888;
+  position: relative;
+  bottom: 85px;
 }
 
-.circle {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 18px;
-  font-weight: bold;
-  color: #fff;
-}
+
 </style>
 
 
-<!-- <script>
-import moviesData from './kinopoisk.json';
 
-export default {
-  data() {
-    return {
-      movies: [],
-      searchQuery: '',
-      itemsPerPage: 20,
-      currentPage: 1,
-    };
-  },
-  computed: {
-    filteredMovies() {
-      const query = this.searchQuery.toLowerCase();
-      return this.movies.filter(movie => {
-        return movie.name.toLowerCase().includes(query);
-      });
-    },
-    displayedMovies() {
-      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-      return this.filteredMovies.slice(startIndex, startIndex + this.itemsPerPage);
-    },
-    shouldShowLoadMoreButton() {
-      return this.currentPage * this.itemsPerPage < this.filteredMovies.length;
-    },
-  },
-  mounted() {
-    this.movies = moviesData.docs;
-  },
-  methods: {
-    loadMore() {
-      this.currentPage++;
-    },
-    getColor(rating) {
-      if (rating < 5) {
-        return "#ff0000"; // red
-      } else if (rating < 7) {
-        return "#ffa500"; // orange
-      } else {
-        return "#008000"; // green
-      }
-    },
-  },
-};
-</script> -->
 
 
 
