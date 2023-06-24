35bf723f7499ad79448b04b62d7749e9efd7491e
NickPavlovskii <84456340+NickPavlovskii@users.noreply.github.com>
2023-06-25 01:15:32 +0300

    сейчас смотрят

--- kinox-app/src/components/main/WatchNow.vue
+++ kinox-app/src/components/main/WatchNow.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="container">
     
-        <h1 class="title">Смотрят сейчас <font-awesome-icon icon="arrow-right-long" class="longArrow" /></h1>
+        <h2 class="title"> 
+          <font-awesome-icon icon="fire" class="fire" 
+         /> 
+          Новинки
+        </h2>
         <div class="wrapper">
            
           <i id="left"  @click="scrollLeft"><font-awesome-icon icon="arrow-left" /></i>
@@ -9,56 +13,9 @@
             @mouseup="dragStop" @touchend="dragStop" :class="{'dragging': isDragging}">
             
             <div class="movie-poster" v-for="movie in latestMovies" :key="movie.id">
-                <router-link to="/" style="  cursor: pointer;text-decoration: none; list-style-type: none;">
-                    <div class="movie-poster">
-              <img :src="movie.poster.url" alt="Постер фильма" class="poster-image" />
-              <!-- <font-awesome-icon icon="heart" class="heart" /> -->
-            </div>
-          
-
-            <div class="movie-details">
-                <circle-progress class="circle_progress"
-                :viewport="true"
-            
-                :on-viewport= "movie.rating.kp.toFixed(1)"
-                    :size="60"
-                    :background="'white'"
-          :is-gradient="true"
-          :percent=movie.rating.kp*10
-          :gradient="{
-            angle: 90,
-            startColor: '#ff0000',
-            stopColor: '#ffff00'
-        }"
-        :is-bg-shadow="true"
-          :bg-shadow="{
-            inset: true,
-            vertical: 2,
-            horizontal: 2,
-            blur: 4,
-            opacity: .4,
-            color: '#000000'
-        }"
-        :border-width="5"
-          :border-bg-width="5"
-      >
-    
-            
-      </circle-progress>
-      <span  class="ratingtext">
-            {{ movie.rating.kp.toFixed(1) }}
-          </span>  
-    
-                <h2 class="movie-name">{{ movie.name }} </h2>
-                 <!-- ({{ movie.type}}) -->
-                <div class="movie-info">
-                  <div class="circleRating">
-                   
-         </div>
-                  <div class="year">{{ movie.year }}</div>
-                </div>
-              </div>
-            </router-link>
+               
+                  <MovieCard :movie="movie" />
+           
             </div>
       
           </div>
@@ -66,23 +23,221 @@
         </div>
     </div>
       </template>
-    
+      
+    <style  lang="scss">
+    .movie-poster {
+       position: relative;
+       display: inline-block;
+     }
+   
+     .description-overlay {
+       position: absolute;
+       top: 0;
+       left: 0;
+       width: 100%;
+       height: 100%;
+       background-color: rgba(0, 0, 0, 0.8);
+       color: #fff;
+       opacity: 0;
+       transition: opacity 0.3s ease;
+       display: flex;
+       align-items: center;
+       justify-content: center;
+       text-align: center;
+     }
+   
+     .movie-poster:hover .description-overlay {
+       opacity: 1;
+     }
+   
+     .shortDescription {
+       max-width: 100%;
+       padding: 20px;
+       word-wrap: break-word;
+       white-space: normal;
+     }
+   
+     p {
+       word-wrap: break-word;
+     }
+   
+   .fire{
+     color:#f89e00 ;
+     
+   }
+  
+   .title{
+    font-family: cursive;
+    letter-spacing: 0.1em;
+   }
+   .longArrow{
+     width: 20px;
+   position: relative;
+   top: 4px;
+   margin-left: 3px;
+   
+   }
+   .circle_progress{
+     position: absolute;
+    margin-top: 12px;
+     left: 5px;
+     color: #ffffff;
+   
+   }
+   .circle_progress .percentage{
+     color: black;
+   }
+   
+   .ratingtext {
+     position: relative;
+   bottom: 111px;
+   margin-left: 26px;
+     font-size: 21px;
+   color: #020c1b;
+     font-weight: bold;
+     text-align: center;
+   }
+   #left{
+     bottom: 290px;
+   left: 5px;
+     background-color: #020c1b99;
+     color: #ffffff;
+   }
+   #right{
+   
+     bottom: 290px;
+     right: 5px;
+     background-color: #020c1b99;
+     color: #ffffff;
+   }
+   .movie-details {
+     padding-top: 10px;
+     width: 100%;
+     color: white;
+     height: 80px;
+   }
+   .container {
+     max-width: 960px;
+     margin: 0 auto;
+     padding: 20px;
+   
+     color: #fff;
+   }
+         
+         .wrapper {
+         height: 100%;
+       
+         color: #fff;
+            max-width: 960px;
+         margin: 0 auto;
+           display: flex;
+       
+           position: relative;
+         }
+         
+         .wrapper i {
+          
+           height: 44px;
+           width: 44px;
+           color: #343F4F;
+           cursor: pointer;
+           font-size: 1.15rem;
+           position: absolute;
+           text-align: center;
+           line-height: 44px;
+           background: #fff;
+           border-radius: 50%;
+           transform: translateY(-50%);
+           transition: transform 0.1s linear;
+         }
+         
+         .wrapper i:active {
+           transform: translateY(-50%) scale(0.9);
+         }
+         
+         .wrapper i:hover {
+           background: #f2f2f2;
+         }
+         
+         .wrapper i:first-child {
+           left: -22px;
+           display: none;
+         }
+         
+         /* .wrapper i:last-child {
+           right: -22px;
+         } */
+         
+         .wrapper .carousel {
+       
+           
+           cursor: pointer;
+           overflow: hidden;
+           white-space: nowrap;
+           scroll-behavior: smooth;
+         }
+         
+         .carousel.dragging {
+           cursor: grab;
+           scroll-behavior: auto;
+         }
+         
+       
+         
+         .carousel .movie-poster {
+        
+         border-radius: 8px;
+           display: inline-block;
+   
+           
+         }
+         
+         .carousel .movie-poster img {
+           
+         border-radius: 11px;
+         width: 215px;
+   
+         }
+         
+         .carousel .movie-poster:first-child {
+           margin-left: 0px;
+         }
+         
+         @media screen and (max-width: 900px) {
+       .carousel .movie-poster {
+         margin-right: 10px; /* Adjust the margin-right value to reduce spacing */
+       }
+     }
+   
+     @media screen and (max-width: 550px) {
+       .carousel .movie-poster img {
+         width: 100%;
+       }
+   
+       .carousel .movie-poster {
+         margin-right: 10px; /* Adjust the margin-right value to reduce spacing */
+       }
+     }
+         </style>
+
+
       <script>
       import { mapState, mapMutations  } from 'vuex';
       import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
       import { library } from '@fortawesome/fontawesome-svg-core'
-      import CircleProgress from "vue3-circle-progress";
-      import { faArrowRight,faArrowLeft, faArrowRightLong, faHeart } from '@fortawesome/free-solid-svg-icons'
+    
+      import { faArrowRight,faArrowLeft, faFire, faHeart } from '@fortawesome/free-solid-svg-icons'
+      import MovieCard from '../MovieCard.vue';
       library.add(faArrowRight)
       library.add(faArrowLeft)
       library.add(faHeart)
-      library.add(faArrowRightLong)
+      library.add(faFire)
    
       export default {
         components: {
             FontAwesomeIcon,
-        CircleProgress,
-        
+      
+        MovieCard
       },
         computed: {
             ...mapState(['movies']),
@@ -165,179 +320,3 @@
       };
       </script>
 
-<style scoped>
-.title{
-  
-}
-.longArrow{
-  width: 20px;
-position: relative;
-top: 4px;
-margin-left: 3px;
-
-}
-.circle_progress{
-  position: absolute;
- margin-top: 12px;
-  left: 5px;
-  color: #ffffff;
-
-}
-.circle_progress .percentage{
-  color: black;
-}
-
-.ratingtext {
-  position: relative;
-bottom: 111px;
-margin-left: 26px;
-  font-size: 21px;
-color: #020c1b;
-  font-weight: bold;
-  text-align: center;
-}
-#left{
-  bottom: 290px;
-left: 5px;
-  background-color: #020c1b99;
-  color: #ffffff;
-}
-#right{
-
-  bottom: 290px;
-  right: 5px;
-  background-color: #020c1b99;
-  color: #ffffff;
-}
-.movie-details {
-  padding-top: 10px;
-  width: 100%;
-  color: white;
-  height: 80px;
-}
-.container {
-  max-width: 960px;
-  margin: 0 auto;
-  padding: 20px;
-
-  color: #fff;
-}
-        .circle_progress{
-  position: absolute;
-  bottom: 70px;
-  left: 10px;
-  color: #ffffff;
-
-}
-.movie-name {
-  font-size: 19px;
-  margin-bottom: 5px;
-  position: relative;
-  bottom: 98px;
-  display: block;
-  width: 250px;
-  font-weight: bold;
-  word-break: break-all;
-}
-
-
-
-
-
-.year {
-  font-size: 16px;
-  color: #888;
-  position: relative;
-  bottom: 95px;
-} 
-      .wrapper {
-      height: 100%;
-    
-      color: #fff;
-         max-width: 960px;
-      margin: 0 auto;
-        display: flex;
-    
-        position: relative;
-      }
-      
-      .wrapper i {
-       
-        height: 44px;
-        width: 44px;
-        color: #343F4F;
-        cursor: pointer;
-        font-size: 1.15rem;
-        position: absolute;
-        text-align: center;
-        line-height: 44px;
-        background: #fff;
-        border-radius: 50%;
-        transform: translateY(-50%);
-        transition: transform 0.1s linear;
-      }
-      
-      .wrapper i:active {
-        transform: translateY(-50%) scale(0.9);
-      }
-      
-      .wrapper i:hover {
-        background: #f2f2f2;
-      }
-      
-      .wrapper i:first-child {
-        left: -22px;
-        display: none;
-      }
-      
-      /* .wrapper i:last-child {
-        right: -22px;
-      } */
-      
-      .wrapper .carousel {
-    
-        
-        cursor: pointer;
-        overflow: hidden;
-        white-space: nowrap;
-        scroll-behavior: smooth;
-      }
-      
-      .carousel.dragging {
-        cursor: grab;
-        scroll-behavior: auto;
-      }
-      
-    
-      
-      .carousel .movie-poster {
-     
-      border-radius: 8px;
-        display: inline-block;
-
-        
-      }
-      
-      .carousel .movie-poster img {
-        
-      border-radius: 11px;
-      width: 215px;
-
-      }
-      
-      .carousel .movie-poster:first-child {
-        margin-left: 0px;
-      }
-      
-      @media screen and (max-width: 900px) {
-        .carousel .movie-poster img {
-          width: calc(100% / 2);
-        }
-      }
-      
-      @media screen and (max-width: 550px) {
-        .carousel .movie-poster img {
-          width: 100%;
-        }
-      }
-      </style>
